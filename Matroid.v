82dfd9da705a326a0570eb2df2223c752c8466db
nkbalacha <nitinkb2015@gmail.com>
2019-12-14 18:37:59 -0500

    Update Matroid.v

--- Matroid.v
+++ Matroid.v
@@ -46,3 +46,63 @@ Theorem Bases_Same_Size : forall A B (M : Matroid) n m, Base M A -> Base M B -> 
 Proof.
   intros. unfold Base in H. unfold Base in H0. destruct H. destruct H0. destruct M. simpl in *. apply I3_iff_I3' in M6. unfold I3' in M6. eapply M6. apply H. apply H0. apply H1. apply H2.
   Qed.
+
+Definition Circuits (M : Matroid) : Ensemble (Ensemble U) :=
+(fun S : Ensemble U =>(~ I M S) /\(forall T : Ensemble U,
+Strict_Included U T S -> I M T)).
+
+(*The following are Circuit principles *)
+Definition C1 : Ensemble (Ensemble U) -> Prop :=
+(fun C => ~ C (Empty_set U)).
+
+Definition C2 : Ensemble (Ensemble U) -> Prop :=
+(fun C => forall (A B : Ensemble U),
+(C A) /\ (C B) /\ Included U A B -> Same_set U A B).
+
+Definition C3 : Ensemble (Ensemble U) -> Prop :=
+(fun C => forall (A B : Ensemble U), forall (e : U),
+(C A) /\ (C B) /\ (~ Same_set U A B) /\
+(In U (Intersection U A B) e) ->
+exists C3 : Ensemble U,
+(C C3) /\
+Included U C3 (Setminus U (Union U A B) (Singleton U e))).
+
+Lemma Circuits_Satisfy_C1 : forall M : Matroid, C1 (Circuits M).
+Proof.
+  unfold C1.
+  intros.
+  intro.
+  unfold Circuits in H.
+  destruct H.
+  contradict H.
+  apply M1.
+Qed.
+
+Lemma Circuits_Satisfy_C2 : forall M : Matroid, C2 (Circuits M).
+Proof.
+  unfold C2.
+  intros.
+  destruct H as [C_A [C_B A_sub_B]].
+  assert (G: A = B \/ A <> B). apply classic.
+  destruct G.
+  apply (Extension U); assumption.
+  unfold Circuits in C_A.
+  unfold Circuits in C_B.
+  unfold Strict_Included in C_A.
+  unfold Strict_Included in C_B.
+  destruct C_A as [A_not_I A_prop_subs_I].
+  destruct C_B as [B_not_I B_prop_subs_I].
+  assert (G: Included U A B /\ A <> B -> I M A).
+  apply B_prop_subs_I.
+  contradict A_not_I.
+  auto.
+Qed.
+
+
+Definition Base (M : Matroid) (B : Ensemble U) :=
+  Maximal (I M) B /\ (I M) B.
+
+Theorem Bases_Same_Size : forall A B (M : Matroid) n m, Base M A -> Base M B -> cardinal U A n -> cardinal U B m -> n = m.
+Proof.
+  intros. unfold Base in H. unfold Base in H0. destruct H. destruct H0. destruct M. simpl in *. apply I3_iff_I3' in M6. unfold I3' in M6. eapply M6. apply H. apply H0. apply H1. apply H2.
+  Qed.
